9decff65a09872fc802088c86c3097a0224c94a9
Ryan <ryan2.lin@hwacom.com>
2021-12-02 10:50:01 +0800

    first commit

---
+++ src/Entrust/EntrustRole.php
@@ -0,0 +1,38 @@
+<?php namespace Hwacom\Entrust;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Hwacom\Entrust
+ */
+
+use Hwacom\Entrust\Contracts\EntrustRoleInterface;
+use Hwacom\Entrust\Traits\EntrustRoleTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Config;
+
+class EntrustRole extends Model implements EntrustRoleInterface
+{
+    use EntrustRoleTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * Creates a new instance of the model.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = Config::get('entrust.roles_table');
+    }
+
+}
